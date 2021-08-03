8e408df4b70198b7bde10a4094aa3597a95ce493
smsirmek <suttipong.mek@gmail.com>
2021-08-03 16:32:34 +0700

    Add Navbar For window and Mobile

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCBxMKZHoVI3SHD7hcZO0xxivjg8n3uTGY",
+    authDomain: "project-879bc.firebaseapp.com",
+    projectId: "project-879bc",
+    storageBucket: "project-879bc.appspot.com",
+    messagingSenderId: "14933523311",
+    appId: "1:14933523311:web:7a28c4efb1be6a94f2c69a"
+  };
+  
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+  export { timestamp };
+  export default firebaseApp.firestore();
